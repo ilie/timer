2500bd11adccc03a51cf38783c476f33a67c75f9
Ilie Florea <floreailie87@gmail.com>
2021-12-21 21:56:51 +0100

    small ajustments in CSS. Changed main logos with SVG files

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-import VLEC from "../../Assets/img/Virginia_Lyons.png";
-import CAMBRIDGE from "../../Assets/img/Cambridge_Platinum_Centre_2.png";
+import VLEC from "../../Assets/img/Virginia_Lyons_H.svg";
+import CAMBRIDGE from "../../Assets/img/Authorised_Platinum_Exam_centre_logo.svg";
 const Header = () => {
   return (
     <header className="app-header">
