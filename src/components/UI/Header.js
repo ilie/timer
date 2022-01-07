102c29d9c0fc2cd24eabe91c038d74622b03640b
Ilie Florea <floreailie87@gmail.com>
2022-01-08 00:54:07 +0100

    Logo Fix

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,11 +1,16 @@
 import React from "react";
 import VLEC from "../../Assets/img/Virginia_Lyons_H.svg";
-import CAMBRIDGE from "../../Assets/img/Cambridge_Platinum_Exam_Centre.svg";
+import CAMBRIDGE from "../../Assets/img/Authorised-Platinum-Exam-centre-logo.svg";
 const Header = () => {
   return (
     <header className="app-header">
       <img src={VLEC} alt="Virginia Lyons Exam Centre" />
-      <img src={CAMBRIDGE} alt="Authorized Platinum Exam Centre" />
+      <img
+        src={CAMBRIDGE}
+        alt="Authorized Platinum Exam Centre"
+        height="125px"
+        width="344px"
+      />
     </header>
   );
 };
