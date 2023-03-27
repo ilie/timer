b808fc7b53075f36729a4fca5cbbdc612495f3f0
Ilie Florea <floreailie87@gmail.com>
2023-03-27 12:26:40 +0200

    Updated the new platinum centre logo from 2023

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import VLEC from "../../Assets/img/Virginia_Lyons_H.svg";
 import CAMBRIDGE from "../../Assets/img/Authorised-Platinum-Centre_Logo_RGB_OUTLINED.svg";
+import CAMBRIDGE23 from "../../Assets/img/Cambridge_Platinum_Centre_2023.svg";
 const Header = () => {
   return (
     <header className="app-header">
       <img src={VLEC} alt="Virginia Lyons Exam Centre" />
       <img
-        src={CAMBRIDGE}
+        className="cambridge-logo"
+        src={CAMBRIDGE23}
         alt="Authorized Platinum Exam Centre"
         height="125px"
-        width="344px"
+        // width="344px"
       />
     </header>
   );
